61a6f1143062daa180d6c77eca0981afbfb3b314
Guilherme Dalmarco <guilherme.dalmarco@br.experian.com>
2018-10-13 17:50:15 -0300

    init lib

---
+++ gwa_framework/__init__.py
@@ -0,0 +1,3 @@
+from gwa_framework.version import __version__
+
+__version__ = __version__
---
+++ setup.py
@@ -0,0 +1,38 @@
+import setuptools
+from setuptools import find_packages
+import gwa_framework
+
+
+def long_description():
+    with open('README.md', encoding='utf8') as f:
+        return f.read()
+
+
+setuptools.setup(
+    name='ecs-lib-boleto-python',
+    version=gwa_framework.__version__,
+
+    url='https://bitbucket.org/serasaecs/ecs-lib-boleto-python',
+    description='Biblioteca padrão de framework para aplicações em Python no GWA.',
+    long_description=long_description(),
+    long_description_content_type="text/markdown",
+
+    author='Guilherme Dalmarco',
+    author_email='dalmarco.br@gmail.com',
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+    ],
+
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=[
+        'reportlab>=3.4.0'
+    ],
+    extras_require={},
+)
