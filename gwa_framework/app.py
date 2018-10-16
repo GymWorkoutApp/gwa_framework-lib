cede0028d53e6a3777e291c4ecdf7448b4e4a392
Guilherme Dalmarco <guilherme.dalmarco@br.experian.com>
2018-10-16 10:48:11 -0300

    fix

--- gwa_framework/app.py
+++ gwa_framework/app.py
@@ -7,6 +7,6 @@ from flask import Flask
 class GWAApp(Flask):
 
     def __init__(self, import_name,  **kwargs):
-        os.environ['FLASK_ENV'] = config('ENV')
-        os.environ['FLASK_DEBUG'] = config('DEBUG')
+        os.environ['FLASK_ENV'] = config('ENV', default='hml')
+        os.environ['FLASK_DEBUG'] = config('DEBUG', default=False)
         super(GWAApp, self).__init__(import_name, **kwargs)
