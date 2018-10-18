4cccdcc41cd9300e597d2ab08570197106f1d36b
Guilherme Dalmarco <guilherme.dalmarco@br.experian.com>
2018-10-18 14:42:25 -0300

    fix cast debug

--- gwa_framework/app.py
+++ gwa_framework/app.py
@@ -8,5 +8,6 @@ class GWAApp(Flask):
 
     def __init__(self, import_name,  **kwargs):
         os.environ['FLASK_ENV'] = config('ENV', default='hml')
-        os.environ['FLASK_DEBUG'] = config('DEBUG', default=False)
+        os.environ['FLASK_DEBUG'] = config('DEBUG', default='False')
         super(GWAApp, self).__init__(import_name, **kwargs)
+''
