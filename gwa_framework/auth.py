706d8b4ebd13f804bc42b89785741669f26681fc
Guilherme Dalmarco <guilherme.dalmarco@br.experian.com>
2018-10-28 07:53:48 -0300

    create middleware

--- gwa_framework/auth.py
+++ gwa_framework/auth.py
@@ -19,8 +19,11 @@ class GWAAuth(object):
         def authenticate():
             if 'Authorization' in request.headers:
                 bearer_token = request.headers.get("Authorization", default=None)
-                requests.post(f'{GWAAuthSettings.INTROSPECT_URL}?'
-                              f'token={token[]}')
+                if bearer_token:
+                    response = requests.post(f'{GWAAuthSettings.INTROSPECT_URL}?token={bearer_token.split("Bearer ")[1]}')
+                    if response.status_code == 200:
+                        token = response.content.decode()
+                        return
             raise NotAuthorizedException('You are not authorized for this request.')
 
         app.before_request_funcs.setdefault(None, []).append(authenticate)
