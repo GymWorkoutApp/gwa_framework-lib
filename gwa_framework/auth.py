fec245c725616a2c0e413379d3d33ab9c472adc6
Guilherme Dalmarco <guilherme.dalmarco@br.experian.com>
2018-12-17 14:39:11 -0200

    fix auth

--- gwa_framework/auth.py
+++ gwa_framework/auth.py
@@ -3,10 +3,8 @@ from typing import Union
 
 import aiohttp
 import jwt
-import requests
 from flask import request, Blueprint
 
-from gwa_framework import asyncio
 from gwa_framework.app import GWAApp
 from gwa_framework.asyncio import get_loop
 from gwa_framework.exceptions.not_authorized import NotAuthorizedException
@@ -15,6 +13,7 @@ from gwa_framework.settings import GWAAuthSettings
 
 class GWAAuth(object):
     app = None
+    logger = None
 
     def __init__(self, app: Union[GWAApp, Blueprint]):
         if app:
@@ -22,17 +21,19 @@ class GWAAuth(object):
 
     def init_app(self, app):
         self.app = app
+        self.logger = app.logger
 
         async def call_oauth_token(bearer_token):
             async with aiohttp.ClientSession() as session:
                 try:
-                    response = await session.post(f'{GWAAuthSettings.INTROSPECT_URL}?token={bearer_token.split("Bearer ")[1]}', "")
+                    response = await session.get(
+                        f'{GWAAuthSettings.INTROSPECT_URL}?token={bearer_token.split("Bearer ")[1]}')
                     if response.status == 200:
                         return await response.json()
 
                     return None
 
-                except asyncio.TimeoutError as e:
+                except TimeoutError as e:
                     self.logger.error(f'TimeoutError endpoint: {GWAAuthSettings.INTROSPECT_URL}')
                     raise e
                 except Exception as e:
@@ -43,9 +44,9 @@ class GWAAuth(object):
             if 'Authorization' in request.headers:
                 bearer_token = request.headers.get("Authorization", default=None)
                 if bearer_token:
-                    resp = get_loop().run_until_complete(call_oauth_token())
+                    resp = get_loop().run_until_complete(call_oauth_token(bearer_token))
 
-                    if resp.status_code == 200:
+                    if resp:
                         token = resp.content.decode()
                         token = json.loads(token)
                         decoded = jwt.decode(token['access_token'], GWAAuthSettings.TOKEN_KEY,
