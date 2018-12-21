25712d53f9e844bdfc9988f316a550b640ffa935
Guilherme Dalmarco <guilherme.dalmarco@br.experian.com>
2018-12-21 14:06:23 -0200

    fix pusub

--- gwa_framework/utils/enums.py
+++ gwa_framework/utils/enums.py
@@ -5,3 +5,12 @@ class PubSubOperation(Enum):
     CREATE = 'create'
     UPDATE = 'update'
     DELETE = 'delete'
+
+    def is_create(self) -> bool:
+        return self.value == self.CREATE
+
+    def is_update(self) -> bool:
+        return self.value == self.UPDATE
+
+    def is_delete(self) -> bool:
+        return self.value == self.DELETE
