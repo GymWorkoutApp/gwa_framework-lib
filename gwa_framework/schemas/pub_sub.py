5234cfc4e82e1970cd6df7711c3aa2d3bf9bc9ff
Guilherme Dalmarco <guilherme.dalmarco@br.experian.com>
2018-12-21 10:46:57 -0200

    fix pusub

--- gwa_framework/schemas/pub_sub.py
+++ gwa_framework/schemas/pub_sub.py
@@ -1,9 +1,10 @@
-from schematics.types import StringType, ModelType
+from schematics.contrib.enum_type import EnumType
+from schematics.types import ModelType
 
 from gwa_framework.schemas.base import BaseSchema
+from gwa_framework.utils.enums import PubSubOperation
 
 
 class PubSubMessage(BaseSchema):
     message = ModelType(model_spec=BaseSchema, required=True, serialized_name='message')
-    operation = StringType(required=True, serialized_name='operation')
-
+    operation = EnumType(required=True, serialized_name='operation', choices=[e.value for e in PubSubOperation])
